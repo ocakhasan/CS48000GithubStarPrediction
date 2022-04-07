78dff3611983ae62a251fb23b87214c06821f58b
fatih <fatihdurmaz@sabanciuniv.edu>
2022-04-07 17:49:04 +0300

    Data status retriever updated. requests fixed
    Signed-off-by: fatih <fatihdurmaz@sabanciuniv.edu>

--- gitCollector.py
+++ gitCollector.py
@@ -23,9 +23,17 @@ class gitCollector ():
             'Authorization': f'token {self.token}',
             'accept': 'application/vnd.github.v3+json'
         }
-        params = {'q': f'?q=language:{self.lang}+stars:{self.stars}&order=desc&per_page=100&page={page}'}
+        #params = {'q': f'=language:{self.lang}+stars:{self.stars}&order=desc&per_page=100&page={page}'}
+        params = {
+            'q': f'language:{self.lang}+stars:{self.stars}',
+            'sort': 'stars',
+            'per_page': 100,
+            'page': {page}
+        }
+
+        payload_str = "&".join("%s=%s" % (k, v) for k, v in params.items())
         try:
-            r = requests.get(URL, headers=headers, params=params)
+            r = requests.get(URL, headers=headers, params=payload_str)
 
             if r.status_code != 200:
                 return None, Exception(f"expected to get HTTP 200, got HTTP {r.status_code}, URL: {r.url}")
@@ -36,7 +44,7 @@ class gitCollector ():
         
     def getStatsOfRepo(self, repoObj):
         stats = {}
-        stats["name"] = repoObj["name"]
+        stats["name"] = repoObj["full_name"]
         stats["url"] = repoObj["html_url"]
         stats["star_count"] = repoObj["stargazers_count"]
         if repoObj["license"] is None:
@@ -46,13 +54,17 @@ class gitCollector ():
         contributorCount, err = self.getContributorCountOfRepo(repoObj["contributors_url"])
         if err == None:
             stats["contributor_count"] = contributorCount
+        stats["size"] = repoObj["size"]
+        stats["watchers_count"] = repoObj["watchers_count"]
+
         stats["forks"] = repoObj["forks_count"]
-        stats["githubPages"] = repoObj["has_pages"]
+        stats["github_pages"] = repoObj["has_pages"]
         stats["open_issues"] = repoObj["open_issues"]
         stats["topics_count"] = len(repoObj["topics"])
         stats["created_at"] = repoObj["created_at"]
         stats["updated_at"] = repoObj["updated_at"]
-        
+        stats["allow_forking"]= repoObj["allow_forking"]
+
         return stats
 
     def getContributorCountOfRepo(self, contUrl):
