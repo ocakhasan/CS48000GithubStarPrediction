a4395109c36f6b83b19142cec24692b772bdd273
ocakhasan <ocakhasan@sabanciuniv.edu>
2022-05-08 10:31:16 +0000

    Merged in datafolder (pull request #9)
    add data folder
    
    * add data folder

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from re import I
 from gitCollector import gitCollector
 import os
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 import pandas as pd
+import os
+from tqdm import tqdm
 
 
 def main():
@@ -13,19 +18,25 @@ def main():
     min_star_count = 100
     max_star_count = 10000
     star_query = f"{min_star_count}..{max_star_count}"
+
+    cwd = os.getcwd()
+    if not os.path.exists(os.path.join(cwd, "data")):
+        os.mkdir("data")
     
     gitC = gitCollector(lang, star_query, token)
 
-    
-    for i in range(1,3):
+    data_folder = os.path.join(cwd, "data")
+
+    for i in range(1,2):
         df = pd.DataFrame()
         repos, err = gitC.getRepos(i)
         filename = f"{lang}_{min_star_count}_{max_star_count}_{i}.csv"
+        filename = os.path.join(data_folder, filename)
         if err != None:
             print('failed to fetch repositories from GitHub, err: ', err)
             return
 
-        for elem in repos['items']:
+        for elem in tqdm(repos['items']):
             df = df.append(gitC.getStatsOfRepo(elem), ignore_index=True)
 
         df.to_csv(filename)
